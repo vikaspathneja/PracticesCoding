8900512aa554013fda73dabcc2e30247fa3f3198
Vikas <Vikas@vikas42329@gmail.com>
2020-04-01 12:28:52 -0700

    Two Tree Are Identical With Better and Less Line of Code

--- DataStructurePractice/src/TreesQs/TwoTreesAreIdentical.java
+++ DataStructurePractice/src/TreesQs/TwoTreesAreIdentical.java
@@ -144,6 +144,17 @@ class TreeInternal
 		if(root1==null && root2==null)
 			return true;
 		
+		if(root1!=null && root2!=null) 
+		return (root1.data==root2.data) && isIdentical(root1.left, root2.left) && isIdentical(root1.right, root2.right);
+				
+		return false;
+	}
+	
+	boolean isIdenticalOld(Node root1, Node root2)
+	{
+		if(root1==null && root2==null)
+			return true;
+		
 		if(root1!=null && root2==null)
 			return false;
 		
