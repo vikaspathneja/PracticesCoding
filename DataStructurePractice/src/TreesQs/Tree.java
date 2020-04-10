a3546402a3feebf5c1de8befdcdaacd91eed0f0e
Vikas <Vikas@vikas42329@gmail.com>
2020-04-10 06:41:55 -0700

    Max Path Sum

--- DataStructurePractice/src/TreesQs/Tree.java
+++ DataStructurePractice/src/TreesQs/Tree.java
@@ -7,6 +7,14 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
+class summ {
+	int sumSoFar;
+}
+
+class Res {
+	public int val;
+}
+
 public class Tree {
 
 	private static Node root;
@@ -14,10 +22,9 @@ public class Tree {
 	private static int mindistance = 0;
 	private static int maxdistance = 0;
 	private static HashMap<Integer, ArrayList<Integer>> hm;
-	
+
 	private static int count;
-	
-	
+
 	private Node head;
 	private static Node prev;
 
@@ -25,33 +32,90 @@ public class Tree {
 
 	private static int maxHeight;
 
-	
-	
+	/**
+	 * @see to find the max sum of path in tree
+	 * @param root node of tree
+	 * @return maximum Sum of path
+	 */
+	int maxPathSum(Node node) {
 
-	/** 
+		// Initialize result
+		// int res2 = Integer.MIN_VALUE;
+		Res res = new Res();
+		res.val = Integer.MIN_VALUE;
+
+		// Compute and return result
+		maxPathSumUtil(node, res);
+		return res.val;
+	}
+
+	// This function returns overall maximum path sum in 'res'
+	// And returns max path sum going through root.
+	int maxPathSumUtil(Node node, Res res) {
+
+		// Base cases
+		if (node == null)
+			return 0;
+		if (node.left == null && node.right == null)
+			return node.data;
+
+		// Find maximum sum in left and right subtree. Also
+		// find maximum root to leaf sums in left and right
+		// subtrees and store them in ls and rs
+		int ls = maxPathSumUtil(node.left, res);
+		int rs = maxPathSumUtil(node.right, res);
+
+		// If both left and right children exist
+		if (node.left != null && node.right != null) {
+
+			// Update result if needed
+			res.val = Math.max(res.val, ls + rs + node.data);
+
+			// Return maxium possible value for root being
+			// on one side
+			int val = Math.max(ls, rs) + node.data;
+			return val;
+		}
+
+		// If any of the two children is empty, return
+		// root sum for root being on one side
+		int mainmax=(node.left == null) ? rs + node.data : ls + node.data;
+	return mainmax;
+	}
+
+	int MaxSumUtil(Node root, Res res) {
+		if (root == null)
+			return 0;
+
+		if (root.left == null && root.right == null)
+			return root.data;
+
+		int left = MaxSumUtil(root.left, res);
+		int right = MaxSumUtil(root.right, res);
+
+		if (root.left != null && root.right != null) {
+			res.val = Math.max(res.val, left + right + root.data);
+			return Math.max(left, right) + root.data;
+		}
+
+		return root.left != null ? left + root.data : right + root.data;
+	}
+
+	/**
 	 * 
 	 * @param root
 	 * @return max distance of Tree any node to any node
 	 */
 	public static int diameterofTree(Node root) {
 //		 return Math.max(maxheight(root.left)+maxheight(root.right),Math.max(diameterofTree(root.left),diameterofTree(root.right)));
-		if(root==null)
+		if (root == null)
 			return 0;
-		
-		return Math.max(
-				(1+height(root.left)+height(root.right)),
-				 Math.max(
-						 diameterofTree(root.left),
-						 diameterofTree(root.right)
-						 )
-				 );
 
-	 }
-	
-	
-	
-	
-	
+		return Math.max((1 + height(root.left) + height(root.right)),
+				Math.max(diameterofTree(root.left), diameterofTree(root.right)));
+
+	}
+
 	public void leftView(Node root) {
 		if (root != null) {
 			System.out.print(root.data + " ");
@@ -505,7 +569,7 @@ public class Tree {
 //	static int maxHeight;
 
 	public static int maxheight(Node root) {
-		maxHeight=0;
+		maxHeight = 0;
 		return maxheight(root, maxHeight);
 	}
 
