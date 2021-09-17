64e36d396bad4ec83c895e4fd36bcd415f92180f
“Vikas <vikaspathneja@gmail.com>
2021-09-17 14:33:00 +0530

    Updated

--- src/custom_static_examples/StaticMethodsInInheritence.java
+++ src/custom_static_examples/StaticMethodsInInheritence.java
@@ -32,11 +32,32 @@ class ManangerEmp extends Employee{
 
 public class StaticMethodsInInheritence {
 	 public static void main(String[] args) {
+		
+		/**
+		 * as static method belongs to class level , Employee ref is being used to 
+		 * invoke static methods so employee class static method getOtherDetails 
+		 * will be invoked 
+		 */
 		 Employee empl=new ManangerEmp();
 		 empl.getOtherDetails();
 
-		 ManangerEmp empl2=null;
+		 
+		 /**
+		  * Here ManangerEmp  class ref is used to invoke static method
+		  * so ManangerEmp class static method will be invoked
+		  */
+		 ManangerEmp empl2=new ManangerEmp();
 		 empl2.getOtherDetails();
 
+		 /**
+		  * ManangerEmp empl3=null; 
+		  * even though above ref is ref to null, still it will be able to invoke
+		  * static method of ManangerEmp class ,as at the class loading time
+		  * static methods will be loaded as well 
+		  */
+		 ManangerEmp empl3=null;
+		 empl3.getOtherDetails();
+
+		 
 	 }
 }
