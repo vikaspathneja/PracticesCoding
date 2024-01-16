475c5e20a9d5e0a5cd855a3bb7df54389a29df19
vik1 <vikaspathneja@gmail.com>
2024-01-16 21:26:50 +0530

    Executor Service Example

--- src/ExectorServiceExample/ExampleExecutorService.java
+++ src/ExectorServiceExample/ExampleExecutorService.java
@@ -1,21 +1,18 @@
 package ExectorServiceExample;
 
-import jdk.nashorn.internal.codegen.CompilerConstants;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
-import java.util.stream.Stream;
 
 public class ExampleExecutorService {
     public static void main(String[] args) throws InterruptedException, ExecutionException, TimeoutException {
-    ExecutorService executorService= Executors.newFixedThreadPool(8);
-        Random r=new Random();
+        ExecutorService executorService = Executors.newFixedThreadPool(8);
+        Random r = new Random();
 
 
         Callable<Integer> integerCallable = () -> {
-            System.out.println(Thread.currentThread().getName()+" started");
+            System.out.println(Thread.currentThread().getName() + " started");
             Thread.sleep(2000);
             int val = r.nextInt(10);
             System.out.println("val=" + val);
@@ -23,41 +20,39 @@ public class ExampleExecutorService {
         };
 
 
-
-        List<Future<Integer>> futures=executorService.invokeAll(Arrays.asList(integerCallable,integerCallable,integerCallable,integerCallable));
-        futures.parallelStream().forEach(future-> {
-            System.out.println(Thread.currentThread().getName()+" inside iterator");
-                    if (future.isDone()) {
-                        System.out.println(Thread.currentThread().getName()+" task done");
-                        try {
-                            System.out.println("future.get()=="+future.get());
-                        } catch (InterruptedException | ExecutionException e) {
-                            throw new RuntimeException(e);
-                        }
-                    } else {
-                        System.out.println(Thread.currentThread().getName()+" task not done yet");
+        List<Future<Integer>> futures = executorService.invokeAll(Arrays.asList(integerCallable, integerCallable, integerCallable, integerCallable));
+        futures.parallelStream().forEach(future -> {
+            System.out.println(Thread.currentThread().getName() + " inside iterator");
+            if (future.isDone()) {
+                System.out.println(Thread.currentThread().getName() + " task done");
+                try {
+                    System.out.println("future.get()==" + future.get());
+                } catch (InterruptedException | ExecutionException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                System.out.println(Thread.currentThread().getName() + " task not done yet");
 //                        try {
 ////                            Thread.sleep(1000);
 //                        } catch (InterruptedException e) {
 //                            throw new RuntimeException(e);
 //                        }
 //            if(future.isDone()){
-                        System.out.println(Thread.currentThread().getName()+"Task retried after wait");
-                        try {
-                            Integer values = future.get(1000, TimeUnit.MILLISECONDS);
-                        } catch (TimeoutException | InterruptedException | ExecutionException e) {
-                            System.out.println(Thread.currentThread().getName()+"Task will be cancelled now as response not received in after retried as well 1 sec");
-                            future.cancel(true);
-                            if (future.isCancelled()) {
-                                System.out.println(Thread.currentThread().getName()+" task cancelled");
-                            }
-                        }
-//            }
+                System.out.println(Thread.currentThread().getName() + "Task retried after wait");
+                try {
+                    Integer values = future.get(1000, TimeUnit.MILLISECONDS);
+                } catch (TimeoutException | InterruptedException | ExecutionException e) {
+                    System.out.println(Thread.currentThread().getName() + "Task will be cancelled now as response not received in after retried as well 1 sec");
+                    future.cancel(true);
+                    if (future.isCancelled()) {
+                        System.out.println(Thread.currentThread().getName() + " task cancelled");
                     }
                 }
-        );
+//            }
+            }
+        });
 //        Thread.sleep(50000);
-        futures.forEach(future-> {
+        futures.forEach(future -> {
             try {
                 if (future.isDone()) {
                     System.out.println(Thread.currentThread().getName() + " at the end of program when task done");
@@ -67,10 +62,11 @@ public class ExampleExecutorService {
                 if (future.isCancelled()) {
                     System.out.println(Thread.currentThread().getName() + " at the end of program when task cancelled");
                 }
-            }catch(Exception ee){
+            } catch (Exception ee) {
                 System.out.println("something got wrong in coding");
             }
         });
 
 
-}}
+    }
+}
